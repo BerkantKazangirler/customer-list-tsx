65caf7979b8bd38c721b0925b7dcfe105fbba62d
BerkantKazangirler <berkantkazangirler@gmail.com>
2024-09-06 15:01:47 +0300

    feat: added react-icons library & changed all svgs

--- src/components/buttons/buttons.tsx
+++ src/components/buttons/buttons.tsx
@@ -1,67 +1,27 @@
-import React, { useEffect, useState } from "react";
+import { PiEnvelopeSimple } from "react-icons/pi";
+import { FaRegBell } from "react-icons/fa";
+import { RxCaretDown } from "react-icons/rx";
 function Buttons() {
   return (
     <div className="flex flex-row md:justify-end justify-center relative md:right-5 right-0 py-8 w-full">
       <div className="flex flex-row gap-5">
         <div className="bg-neutral-200 p-4 rounded-xl h-max md:flex hidden">
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M9 16.75C9 16.75 9 19.25 12 19.25C15 19.25 15 16.75 15 16.75M17.25 12V10C17.25 7.1005 14.8995 4.75 12 4.75C9.10051 4.75 6.75 7.10051 6.75 10V12L4.75 16.25H19.25L17.25 12Z"
-              stroke="#212529"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
+          <FaRegBell />
         </div>
         <div className="bg-neutral-200 p-4 rounded-xl h-max md:flex hidden">
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M5.5 6.5L12 12.25L18.5 6.5M4.75 7.75C4.75 6.64543 5.64543 5.75 6.75 5.75H17.25C18.3546 5.75 19.25 6.64543 19.25 7.75V16.25C19.25 17.3546 18.3546 18.25 17.25 18.25H6.75C5.64543 18.25 4.75 17.3546 4.75 16.25V7.75Z"
-              stroke="#212529"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
+          <PiEnvelopeSimple />
         </div>
-        <div className="bg-neutral-200 p-4 rounded-xl h-max flex-row gap-2 md:flex hidden">
+        <div className="bg-neutral-200 p-3 rounded-xl h-max flex-row gap-2 md:flex hidden">
           <img
             src={"./public/user.png"}
             alt="User İmage"
-            className="h-6 rounded"
+            className="h-5 self-center rounded"
           />
-          <span className="text-black font-semibold md:text-lg text-sm">
+          <span className="text-black font-semibold text-base">
             Derek Alvarado
           </span>
           <button className="ml-3">
-            <svg
-              width="8"
-              height="5"
-              viewBox="0 0 8 5"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M7.25 0.75L4 4.25L0.75 0.75"
-                stroke="#ADB5BD"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <RxCaretDown />
           </button>
         </div>
       </div>
--- src/components/customerlist/customerlist.tsx
+++ src/components/customerlist/customerlist.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
+import { IoIosSearch } from "react-icons/io";
 import { Buttons } from "../buttons";
+import { CiFilter } from "react-icons/ci";
+import { IoFilterOutline } from "react-icons/io5";
 
 function CList() {
   const [openCustomerArea, setCustomerArea] = useState(false);
@@ -7,11 +10,11 @@ function CList() {
   return (
     <div className="flex flex-col w-full">
       <Buttons></Buttons>
-      <div className="p-8 mt-5">
+      <div className="px-8 mt-2">
         <span className="font-semibold lg:text-5xl text-2xl">
           Customers' List
         </span>
-        <div className="flex flex-row mt-5 gap-3 lg:text-lg text-sm">
+        <div className="flex flex-row gap-3 lg:text-lg text-sm">
           <span className="opacity-70">Dashboard</span>
           <span>/ Customers' List</span>
         </div>
@@ -25,31 +28,17 @@ function CList() {
             </button>
             {openCustomerArea && (
               <div className="flex flex-col lg:ml-4 relative">
-                <button className="py-5 px-5 border-b bg-neutral-50 rounded-t-xl absolute z-10">
+                <button className="py-5 px-5 border-b bg-neutral-50 rounded-t-xl absolute z-10 lg:w-96 w-60">
                   Importing by a file
                 </button>
-                <button className="py-5 px-5 bg-neutral-50 rounded-b-xl absolute lg:mt-16 mt-20">
+                <button className="py-5 px-5 bg-neutral-50 rounded-b-xl absolute lg:mt-16 mt-20 lg:w-96 w-60">
                   Entering manually
                 </button>
               </div>
             )}
           </div>
-          <div className="">
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="absolute mt-5 ml-5"
-            >
-              <path
-                d="M12.8333 12.8333L10.3333 10.3333M3.16663 7.33329C3.16663 5.03211 5.03211 3.16663 7.33329 3.16663C9.63449 3.16663 11.5 5.03211 11.5 7.33329C11.5 9.63449 9.63449 11.5 7.33329 11.5C5.03211 11.5 3.16663 9.63449 3.16663 7.33329Z"
-                stroke="#ADB5BD"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+          <div>
+            <IoIosSearch className="absolute mt-5 ml-5" />
             <input
               type="text"
               className="bg-neutral-100 text-black h-14 rounded-xl border lg:w-96 w-60 indent-14"
@@ -57,236 +46,32 @@ function CList() {
             />
           </div>
         </div>
-        <table className="hidden flex-col bg-neutral-50/90 p-8 mt-10 2xl:flex">
+        <table className="flex-col bg-neutral-50/90 p-8 mt-10 flex">
           <tr className="flex flex-row text-[#6C757D]">
             <td className="border-r">
               <span className="pr-10 pl-5">#</span>
             </td>
             <td className="border-r flex flex-row justify-between">
-              <span className="w-64 flex pr-10 pl-7">Full Name</span>
+              <span className="xl:w-64 lg:w-52 flex pr-10 pl-7">Full Name</span>
               <div className="flex flex-row gap-3 my-auto">
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M2.66675 5.16666H13.3334M4.33341 7.83332H11.6667M5.66675 10.5H10.3334"
-                    stroke="#6C757D"
-                    stroke-linecap="round"
-                  />
-                </svg>
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12.8334 3.16666H3.16675L6.20791 6.96812C6.39704 7.20452 6.50008 7.49826 6.50008 7.80106V12.1667C6.50008 12.5349 6.79855 12.8333 7.16675 12.8333H8.83341C9.20161 12.8333 9.50008 12.5349 9.50008 12.1667V7.80106C9.50008 7.49826 9.60315 7.20452 9.79228 6.96812L12.8334 3.16666Z"
-                    stroke="#6C757D"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
+                <IoFilterOutline />
+                <CiFilter />
               </div>
             </td>
-            <td className="border-r flex flex-row justify-between">
+            <td className="border-r hidden flex-row justify-between md:flex">
               <span className="pr-20 pl-7">Status</span>
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="my-auto"
-              >
-                <path
-                  d="M12.8334 3.16666H3.16675L6.20791 6.96812C6.39704 7.20452 6.50008 7.49826 6.50008 7.80106V12.1667C6.50008 12.5349 6.79855 12.8333 7.16675 12.8333H8.83341C9.20161 12.8333 9.50008 12.5349 9.50008 12.1667V7.80106C9.50008 7.49826 9.60315 7.20452 9.79228 6.96812L12.8334 3.16666Z"
-                  stroke="#6C757D"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
+              <CiFilter className="my-auto" />
             </td>
-            <td className="border-r flex flex-row justify-between">
-              <span className="px-10 w-96 flex">E-Mail</span>
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="my-auto"
-              >
-                <path
-                  d="M2.66675 5.16666H13.3334M4.33341 7.83332H11.6667M5.66675 10.5H10.3334"
-                  stroke="#6C757D"
-                  stroke-linecap="round"
-                />
-              </svg>
+            <td className="border-r hidden flex-row justify-between md:hidden lg:flex">
+              <span className="px-10 xl:w-96 lg:w-60 flex">E-Mail</span>
+              <IoFilterOutline className="my-auto" />
             </td>
-            <td className="border-r flex flex-row justify-between">
-              <span className="px-10 w-96 flex">Date of Birth</span>
+            <td className="border-r hidden flex-row justify-between md:hidden lg:hidden xl:flex">
+              <span className="px-10 2xl:w-96 xl:w-80 flex">Date of Birth</span>
               <div className="flex flex-row gap-3 my-auto">
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M2.66675 5.16666H13.3334M4.33341 7.83332H11.6667M5.66675 10.5H10.3334"
-                    stroke="#6C757D"
-                    stroke-linecap="round"
-                  />
-                </svg>
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12.8334 3.16666H3.16675L6.20791 6.96812C6.39704 7.20452 6.50008 7.49826 6.50008 7.80106V12.1667C6.50008 12.5349 6.79855 12.8333 7.16675 12.8333H8.83341C9.20161 12.8333 9.50008 12.5349 9.50008 12.1667V7.80106C9.50008 7.49826 9.60315 7.20452 9.79228 6.96812L12.8334 3.16666Z"
-                    stroke="#6C757D"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
+                <IoFilterOutline />
+                <CiFilter />
               </div>
-            </td>
-          </tr>
-        </table>
-
-        <table className="flex flex-col bg-neutral-50/90 w-fit p-8 mt-10 lg:hidden">
-          <tr className="flex flex-row text-[#6C757D]">
-            <td className="border-r flex flex-row justify-between">
-              <span className="w-32 flex pr-2">Full Name</span>
-              <div className="flex flex-row gap-3 my-auto">
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M2.66675 5.16666H13.3334M4.33341 7.83332H11.6667M5.66675 10.5H10.3334"
-                    stroke="#6C757D"
-                    stroke-linecap="round"
-                  />
-                </svg>
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12.8334 3.16666H3.16675L6.20791 6.96812C6.39704 7.20452 6.50008 7.49826 6.50008 7.80106V12.1667C6.50008 12.5349 6.79855 12.8333 7.16675 12.8333H8.83341C9.20161 12.8333 9.50008 12.5349 9.50008 12.1667V7.80106C9.50008 7.49826 9.60315 7.20452 9.79228 6.96812L12.8334 3.16666Z"
-                    stroke="#6C757D"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </div>
-            </td>
-            <td className="border-r flex flex-row justify-between">
-              <span className="pr-10 pl-7">Status</span>
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="my-auto"
-              >
-                <path
-                  d="M12.8334 3.16666H3.16675L6.20791 6.96812C6.39704 7.20452 6.50008 7.49826 6.50008 7.80106V12.1667C6.50008 12.5349 6.79855 12.8333 7.16675 12.8333H8.83341C9.20161 12.8333 9.50008 12.5349 9.50008 12.1667V7.80106C9.50008 7.49826 9.60315 7.20452 9.79228 6.96812L12.8334 3.16666Z"
-                  stroke="#6C757D"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </td>
-          </tr>
-        </table>
-
-        <table className="hidden flex-col bg-neutral-50/90 w-fit p-8 mt-10 lg:flex">
-          <tr className="flex flex-row text-[#6C757D]">
-            <td className="border-r flex flex-row justify-between">
-              <span className="w-32 flex pr-2">Full Name</span>
-              <div className="flex flex-row gap-3 my-auto">
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M2.66675 5.16666H13.3334M4.33341 7.83332H11.6667M5.66675 10.5H10.3334"
-                    stroke="#6C757D"
-                    stroke-linecap="round"
-                  />
-                </svg>
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12.8334 3.16666H3.16675L6.20791 6.96812C6.39704 7.20452 6.50008 7.49826 6.50008 7.80106V12.1667C6.50008 12.5349 6.79855 12.8333 7.16675 12.8333H8.83341C9.20161 12.8333 9.50008 12.5349 9.50008 12.1667V7.80106C9.50008 7.49826 9.60315 7.20452 9.79228 6.96812L12.8334 3.16666Z"
-                    stroke="#6C757D"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </div>
-            </td>
-            <td className="border-r flex flex-row justify-between">
-              <span className="pr-10 pl-7">Status</span>
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="my-auto"
-              >
-                <path
-                  d="M12.8334 3.16666H3.16675L6.20791 6.96812C6.39704 7.20452 6.50008 7.49826 6.50008 7.80106V12.1667C6.50008 12.5349 6.79855 12.8333 7.16675 12.8333H8.83341C9.20161 12.8333 9.50008 12.5349 9.50008 12.1667V7.80106C9.50008 7.49826 9.60315 7.20452 9.79228 6.96812L12.8334 3.16666Z"
-                  stroke="#6C757D"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </td>
-            <td className="border-r flex flex-row justify-between">
-              <span className="px-10 w-96 flex">E-Mail</span>
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="my-auto"
-              >
-                <path
-                  d="M2.66675 5.16666H13.3334M4.33341 7.83332H11.6667M5.66675 10.5H10.3334"
-                  stroke="#6C757D"
-                  stroke-linecap="round"
-                />
-              </svg>
             </td>
           </tr>
         </table>
